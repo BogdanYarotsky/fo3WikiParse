57796f5601ad25d963c826789623b8909a8b5eb3
BogdanYarotsky <byarotsky@gmail.com>
2024-04-11 22:28:45 +0200

    refactoring table reading

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Text.HTML.TagSoup
+import           Data.Function
+import           Text.HTML.TagSoup
 
 tableClass :: String
 tableClass = "va-table va-table-full va-table-shaded sortable"
@@ -8,13 +9,8 @@ tableClass = "va-table va-table-full va-table-shaded sortable"
 main :: IO ()
 main = do
   html <- readFile "perks.html"
-  let tags = parseTags html
-  let html2 =
-        takeWhile (/= TagClose "table") $
-          dropWhile (/= TagOpen "table" [("class", tableClass)]) $ -- Use tail to drop the first TagOpen that matches the condition
-            tags
-
-  -- Convert the extracted tags back to a string
-  let htmlString = renderTags html2
-
-  putStrLn htmlString
+  let perksTable = parseTags html
+        & dropWhile (/= TagOpen "table" [("class", tableClass)])
+        & takeWhile (/= TagClose "table")
+        & renderTags
+  putStrLn perksTable
